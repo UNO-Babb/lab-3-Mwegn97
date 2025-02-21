2fb9d6b9c815f5ebfb2d3e053387943e20a13354
Mwegn97 <megannwegner@unomaha.edu>
2025-02-21 20:04:56 +0000

    Finished Code

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Megann Wegner
+#Date: 02/05/2025
+#Assignment: Lab 3 Rock, Paper, Scissors
 import random
 
 def main():
@@ -11,10 +11,60 @@ def main():
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+  play = "yes"
+  while play == "yes":
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    #Prompt the user for their RPS selection
+    #Determine winner and state what happened to the user
+    #Ask the user if they would like to play again.
+
+    player = input("Enter choice (rock, paper, scissors): ")
+    
+    computer = random.choice(["rock", "paper", "scissors"])
+
+    if player == "rock":
+      print("Player chose rock.")
+    elif player == "paper":
+      print("Player chose paper.")
+    elif player == "scissors":
+      print("Player chose scissors.")
+    else:
+      print("Invalid option.")
+
+    if player == "rock":
+      if computer == "rock":
+        print("TIE!")
+        ties = ties + 1
+      if computer == "paper":
+        print("You Lose.")
+        losses = losses + 1
+      if computer == "scissors":
+        print("You Win!")
+        wins = wins + 1
+      
+    if player == "paper":
+      if computer == "paper":
+        print("TIE!")
+        ties = ties + 1
+      if computer == "rock":
+        print("You Lose.")
+        losses = losses + 1
+      if computer == "scissors":
+        print("You Win!")
+        wins = wins + 1
+
+    if player == "scissors":
+      if computer == "scissors":
+        print("TIE!")
+        ties = ties + 1
+      if computer == "rock":
+        print("You Lose.")
+        losses = losses + 1
+      if computer == "paper":
+        print("You Win!")
+        wins = wins + 1
+      
+    play = input("Play again? (yes or no): ")
 
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
--- TempConvert.py
+++ TempConvert.py
@@ -1,16 +1,18 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Megann Wegner
+#Date: 02/05/2025
+#Assignment: Lab 3 Temperature Conversion
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  tempF = input("Enter a temperature in Fahrenheit: ")
+  tempF = int(tempF)
+  tempC = (tempF - 32) * 5/9
 
-  tempC = tempF / 2
+  tempC = round(tempC, 2)
 
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
